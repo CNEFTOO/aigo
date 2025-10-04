db9a2213750aafb8bb2f244be26fe3578836cefd
seaung <seaung.c0m7de@protonmail.com>
2025-10-04 18:09:40 +0800

    feat: 时间格式化
    封装时间格式化方法

--- xtime/xtime.go
+++ xtime/xtime.go
@@ -1,6 +1,10 @@
 package xtime
 
-import "time"
+import (
+	"strconv"
+	"strings"
+	"time"
+)
 
 func GetTimestamp() int64 {
 	return time.Now().Unix()
@@ -20,4 +24,24 @@ func GetTimestampMicro() int64 {
 
 func GetTimestampUTC() int64 {
 	return time.Now().UTC().Unix()
+}
+
+func FormatDate(date string) string {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return ""
+	}
+	return t.Format("2006-01-02")
+}
+
+func ConvTime(date string) string {
+	return strings.Replace(date, "-", "", -1)
+}
+
+func ConvTime2Float64(date string) float64 {
+	val, err := strconv.ParseFloat(date, 64)
+	if err != nil {
+		return 0.0
+	}
+	return val
 }
